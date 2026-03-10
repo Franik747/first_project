63ce09ad8b849508c0c6d5b527d3a1b1e3dfddad
Franik747 <abulovamir76@gmail.com>
2026-03-10 18:00:39 +0300

    Update Program.cs

--- game1/Program.cs
+++ game1/Program.cs
@@ -1,1 +1,62 @@
 Console.WriteLine("Добро пожаловать в сногсшибательную игру 'УльтраДупеМегаГигаХроносАгония: Эпоха Древних Богов и Великих Королевств'!");
+
+enemy enemy1 = new enemy()
+enemy1.name = "гэнгстер"
+enemy1.HP = 100;
+enemy1.damage=10;
+enemy1.drop_exp=20; 
+
+enemy enemy2 = new enemy()
+enemy2.name = "отступник света"
+enemy2.HP = 150;
+enemy2.damage=15;
+enemy2.drop_exp=30; 
+
+enemy enemy3 = new enemy()
+enemy3.name = "тролль"
+enemy3.HP = 200;
+enemy3.damage=20;
+enemy3.drop_exp=40; 
+
+enemy enemy4 = new enemy()
+enemy4.name = "Гвардеец дьявола"
+enemy4.HP = 250;
+enemy4.damage=25;
+enemy4.drop_exp=50; 
+
+enemy enemy5 = new enemy()
+enemy5.name = "Дьявол"
+enemy5.HP = 300;
+enemy5.damage= 50;
+enemy5.drop_exp=100; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class enemy
+{
+   string name;
+   int HP;
+   int damage;
+   int drop_exp;
+}
+
